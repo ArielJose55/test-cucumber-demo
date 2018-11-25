278c09280463e882e35f42bfd6342c7fbaaf652f
Ariel Arnedo <arieljosearnedocervantes@gmail.com>
2018-11-25 16:40:59 -0500

    creando la estructura de cucumber

---
+++ src/test/java/com/sophos/challenge/pages/DictionaryFacebookPage.java
@@ -0,0 +1,32 @@
+package com.sophos.challenge.pages;
+
+
+import org.openqa.selenium.support.FindBy;
+
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.pages.PageObject;
+
+public class DictionaryFacebookPage extends PageObject{
+	
+	@FindBy(xpath="//input[@type='text' and @name='email']")
+    private WebElementFacade emailField;
+
+    @FindBy(xpath="//input[@type='password' and @name='pass']")
+    private WebElementFacade passField;
+    
+    @FindBy(xpath="//span[text()='Publicar en Facebook']")
+    private WebElementFacade postButton;
+  
+    
+    public void writeYourEmail(String email) {
+    	emailField.type(email);
+    }
+    
+    public void writeYourPassword(String pass) {
+    	passField.type(pass.concat("\n"));
+    }
+
+    public void postOnFacebook() {
+    	postButton.click();
+    }
+}
---
+++ src/test/java/com/sophos/challenge/steps/serenity/UserSteps.java
@@ -0,0 +1,62 @@
+package com.sophos.challenge.steps.serenity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+
+import com.sophos.challenge.pages.DictionaryFacebookPage;
+import com.sophos.challenge.pages.DictionaryYouTubePage;
+import com.sophos.challenge.steps.util.ReaderBundle;
+
+import net.thucydides.core.annotations.Step;
+
+public class UserSteps {
+
+//	Difinir una referencia de los diccionarios de las paginas a la cuales el actor manejara
+	 DictionaryYouTubePage dictionaryYouTubePage;
+	 DictionaryFacebookPage dictionaryFacebookPage;
+	
+	
+//	 Añadir las acciones del usuario sobre las paginas, que seria metodos con la anotacion @Step
+	
+	 @Step
+	 public void is_the_home_page() {
+		 dictionaryYouTubePage.open();
+	 }
+	 
+	 @Step
+	 public void looks_for_and_start_search(String video) {
+		 dictionaryYouTubePage.look_for_the_video(video.concat(" \n"));
+		 dictionaryYouTubePage.watch_video();
+	 }
+	 
+	 @Step
+	 public void share_it() {
+		 dictionaryYouTubePage.share_it();
+	 }
+	 
+	 public void change_tab() {
+		 WebDriver driver = dictionaryYouTubePage.getDriver();
+		 List<String> list = new ArrayList<>(driver.getWindowHandles());		 
+		 driver.switchTo().window(list.get(list.size() - 1));
+	 }
+	 
+	 @Step
+	 public void log_in_on_facebook() {
+		 dictionaryFacebookPage.writeYourEmail(ReaderBundle.getReader().getStringKey("email"));
+		 dictionaryFacebookPage.writeYourPassword(ReaderBundle.getReader().getStringKey("pass").concat("\n"));
+	 }
+	
+	 public void post_the_video() {
+		
+		 try {
+			 dictionaryFacebookPage.postOnFacebook();
+			 Thread.sleep(3000);
+		 }catch (Exception e) {
+			// TODO: handle exception
+			 e.printStackTrace();
+		}
+	 }
+}
